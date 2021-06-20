25f64e54cb6e40ad2e49a31f5683bb0dc9fac1aa
OBrien-reece <indeche.evans.o@gmail.com>
2021-06-20 11:45:33 +0300

    Learnt about the promises and the CRUD operations in detail

--- mongodb.js
+++ mongodb.js
@@ -1,29 +1,76 @@
-//require mongodb
 const mongodb = require('mongodb')
-//create connecion to mongodb
 const MongoClient = mongodb.MongoClient
+const connectionUrl = "mongodb://127.0.0.1/27017"
+const databaseName = 'robo3t'
 
-//connection url
-const connectionUrl = 'mongodb://127.0.0.1:27017'
-//define the specific database
-const databaseName = 'reece'
-
-MongoClient.connect(connectionUrl, {useUnifiedTopology: true}, (error, client) => {
+MongoClient.connect(connectionUrl, { useUnifiedTopology: true }, (error, client) => {
     if(error){
-        return console.log("failure");
+        return console.log("Unable to connect to the database");
     }
 
     const db = client.db(databaseName)
-
-    db.collection('users').insertOne({
-        name: "reece obrien",
-        age: 45
-    }, (error, result) => {
+    
+    //CREATE 
+    /*
+    db.collection('tasks').insertMany([
+        {
+            task: "Hunting",
+            name: "Evance",
+            age: 23
+        },{
+            task: "Gathering",
+            name: "Rachael",
+            age: 12
+        },{
+            task: "Climbing Trees",
+            name: "Stoneface",
+            age: 45
+        }
+    ], (error, result) => {
         if(error){
-            return console.log("failure");
+            return console.log("There was an error... please try again");
         }
 
-        console.log(result.ops);
+        console.log("Successfuy inserted user data");
     })
-})
+    */
 
+    //READ
+    /*
+    db.collection('tasks').findOne({ task: "Gathering" }, (error, result) => {
+        console.log(result);
+    })
+    */
+
+    /*
+    db.collection('tasks').find({ task: "Gathering" }).count((error, result) => {
+        console.log(result);
+    })
+    */
+
+    //UPDATE
+    /*
+    db.collection('tasks').updateOne({
+        task: "Hunting"
+    },{
+        $inc: {
+            age: 4
+        },
+    }).then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        console.log(error);
+    })
+    */
+   
+    //DELETE
+    /*
+    db.collection('tasks').deleteOne({
+        task: "Hunting"
+    }).then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        console.log(error);
+    })
+    */
+})
